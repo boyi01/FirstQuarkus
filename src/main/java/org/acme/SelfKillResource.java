89d11d71de0999d6fe0d61abdd6b2d4513435f36
boyi <Boyi@gmx.net>
2023-10-12 21:31:25 +0200

    Ad Logging

--- src/main/java/org/acme/SelfKillResource.java
+++ src/main/java/org/acme/SelfKillResource.java
@@ -5,11 +5,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.logging.Logger;
+import org.jboss.resteasy.resteasy_jaxrs.i18n.LogMessages_.logger;
+
 @Path("/selfkill")
 public class SelfKillResource {
+
+    private static final Logger LOG = Logger.getLogger(SelfKillResource.class);
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOG.info("Pod will dye now");
         System.exit(0);
         return "Hello RESTEasyasasdasdasdd";
     }
